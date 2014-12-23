0974f10cac58fb01ffd108c1a4a7daeffa7972be
lbencs <lbencs@hotmail.com>
2014-12-23 13:57:18 +0800

    add

--- UIScrollViewAutolayoutDome/UIScrollViewAutolayoutDome/ViewController.m
+++ UIScrollViewAutolayoutDome/UIScrollViewAutolayoutDome/ViewController.m
@@ -11,7 +11,9 @@
 
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIView *contentView;
+@property (weak, nonatomic) IBOutlet UIView    * contentView;
+@property (weak, nonatomic) UITextField        * activeTextField;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -39,29 +41,60 @@
                                                                       attribute:NSLayoutAttributeRight
                                                                      multiplier:1.0f
                                                                        constant:0];
-//    NSLayoutConstraint *topEdgeAlign = [NSLayoutConstraint constraintWithItem:self.contentView
-//                                                                    attribute:NSLayoutAttributeTop
-//                                                                    relatedBy:0
-//                                                                       toItem:self.view
-//                                                                    attribute:NSLayoutAttributeTop
-//                                                                   multiplier:1.0f
-//                                                                     constant:0];
-//    NSLayoutConstraint *bottomEdgeAlign = [NSLayoutConstraint constraintWithItem:self.contentView
-//                                                                    attribute:NSLayoutAttributeBottom
-//                                                                    relatedBy:0
-//                                                                       toItem:self.view
-//                                                                    attribute:NSLayoutAttributeBottom
-//                                                                   multiplier:1.0f
-//                                                                     constant:0];
-    
     [self.view addConstraints:@[leftEdgeAlign,rightEdgeAlign]];
 }
 
-//- willanimate
+- (void)viewWillAppear:(BOOL)animated{
+    [self registerForKeyboardNotifications];
+}
+- (void)viewWillDisappear:(BOOL)animated{
+    [self deregisterFromKeyboardNotifications];
+}
+
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWasShown:) name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeHidden:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)keyboardWasShown:(NSNotification *)notification{
+    NSDictionary *userInfo = [notification userInfo];
+    CGRect keyboardRect = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    keyboardRect = [self.view convertRect:keyboardRect fromView:nil];
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardRect.size.height, 0.0);
+    self.scrollView.contentInset = contentInsets;
+    self.scrollView.scrollIndicatorInsets = contentInsets;
+    
+    CGRect screenRect = self.view.frame;
+    screenRect.size.height -= keyboardRect.size.height;
+    if (!CGRectContainsPoint(screenRect, self.activeTextField.frame.origin) ) {
+        [self.scrollView scrollRectToVisible:self.activeTextField.frame animated:YES];
+    }
+}
+- (void)keyboardWillBeHidden:(NSNotification *)notification{
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    self.scrollView.contentInset = contentInsets;
+    self.scrollView.scrollIndicatorInsets = contentInsets;
+}
+
+- (void)deregisterFromKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField{
+    self.activeTextField = textField;
+}
+- (void)textFieldDidEndEditing:(UITextField *)textField{
+    self.activeTextField = nil;
+}
 
 - (void)deviceOrientationDidChange:(NSNotification *)notification{
     UIDevice* device = [notification valueForKey:@"object"];
-    NSLog(@"%d",device.orientation);
     switch (device.orientation) {
         case UIDeviceOrientationPortrait:
         case UIDeviceOrientationPortraitUpsideDown:
