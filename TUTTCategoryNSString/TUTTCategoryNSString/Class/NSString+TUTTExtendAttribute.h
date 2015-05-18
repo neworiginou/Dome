1d937911e065bae59f99948e0dade00b422a4b1c
lben <lben@lbens-Mac-mini.local>
2015-05-18 18:27:32 +0800

    修改

---
+++ TUTTCategoryNSString/TUTTCategoryNSString/Class/NSString+TUTTExtendAttribute.h
@@ -0,0 +1,12 @@
+//
+//  NSString+TUTTExtendAttribute.h
+//  TUTTCategoryNSString
+//
+//  Created by lben on 15/5/18.
+//  Copyright (c) 2015年 lben. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (TUTTExtendAttribute)
+@end
---
+++ TUTTCategoryNSString/TUTTCategoryNSString/Class/NSString+TUTTExtendAttribute.m
@@ -0,0 +1,13 @@
+//
+//  NSString+TUTTExtendAttribute.m
+//  TUTTCategoryNSString
+//
+//  Created by lben on 15/5/18.
+//  Copyright (c) 2015年 lben. All rights reserved.
+//
+
+#import "NSString+TUTTExtendAttribute.h"
+
+@implementation NSString (TUTTExtendAttribute)
+
+@end
