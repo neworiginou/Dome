5678163e0f1b15a904933af56790be9e9c593510
鲁文涛 <luwentao@18.net>
2015-06-08 23:19:51 +0800

    Runtime dome

---
+++ NSRunTimeDome/NSRunTimeDome/AlternateObject.m
@@ -0,0 +1,15 @@
+//
+//  AlternateObject.m
+//  NSRunTimeDome
+//
+//  Created by lbencs on 15/6/8.
+//  Copyright (c) 2015年 lbencs. All rights reserved.
+//
+
+#import "AlternateObject.h"
+
+@implementation AlternateObject
+- (void)myMsg:(NSString *)msg{
+    NSLog(@"%@",msg);
+}
+@end
---
+++ NSRunTimeDome/NSRunTimeDome/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSRunTimeDome
+//
+//  Created by lbencs on 15/6/8.
+//  Copyright (c) 2015年 lbencs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
